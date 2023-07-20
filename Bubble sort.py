4923aa83b39b47f314bd1d3b84a739601bae2d9c
Nahom Elias <68940688+nahubn1@users.noreply.github.com>
2023-07-20 14:30:58 +0300

    Create Bubble sort.py

---
+++ Bubble sort.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countSwaps' function below.
+#
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def countSwaps(a):
+    swaps = 0
+    n = len(a)
+    for i in range(n):
+        for j in range(n-1):
+            
+            if a[j] > a[j+1]:
+                temp = a[j]
+                a[j] = a[j+1]
+                a[j+1] = temp
+                
+                swaps += 1
+    
+    print(f'Array is sorted in {swaps} swaps.')
+    print(f'First Element: {a[0]}')
+    print(f'Last Element: {a[-1]}')
+    
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
